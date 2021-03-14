c8cf482c7aab0fbb3c16db97da579b4e197d8775
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-03-14 08:13:40 +0700

    util JWT

---
+++ src/utils/jwt.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import HttpException from '../exceptions/http';
+import { jwtConfig } from '../configs/jwt';
+import { DataStoredInToken } from '../interfaces/auth.interface';
+export function generateToken(user: DataStoredInToken, secretSignature: string, tokenLife: any): string {
+	try {
+		const userData = { _id: user._id, email: user.email };
+		return jwt.sign({ data: userData }, secretSignature, {
+			algorithm: 'HS256',
+			expiresIn: tokenLife,
+		});
+	} catch (error) {
+		throw new HttpException(400, error.message);
+	}
+}
+
+export function verifyToken(token: string, secretKey: string) {
+	try {
+		return jwt.verify(token, secretKey);
+	} catch (error) {
+		throw new HttpException(400, error.message);
+	}
+}
+
+export function returnToken(user: DataStoredInToken) {
+	try {
+		const accessToken = generateToken(user, jwtConfig.ACCESS, jwtConfig.SHORT_TIME);
+		const refreshToken = generateToken(user, jwtConfig.REFRESH, jwtConfig.LONG_TIME);
+		return { accessToken, refreshToken };
+	} catch (error) {
+		throw new HttpException(400, error.message);
+	}
+}
