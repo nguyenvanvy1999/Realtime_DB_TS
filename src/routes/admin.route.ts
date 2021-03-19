433ddb27b822ccf46d24bca1fd6fa8975d2a7bf7
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-03-19 09:54:36 +0700

    add admin controller and router

---
+++ src/routes/admin.route.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import Route from '../interfaces/route.interface';
+import multer from 'multer';
+import { authJWT, checkRole } from '../middlewares/auth.middleware';
+import AdminController from '../controllers/admin.controller';
+
+class AdminRoute implements Route {
+	public path: string = '/admin';
+	public router = Router();
+	constructor() {
+		this.initializeRoutes();
+	}
+	private initializeRoutes() {
+		this.router.use(multer().none());
+		this.router.use(authJWT, checkRole);
+		this.router.post('/clear', AdminController.cleanUser);
+		this.router.get('/users', AdminController.getAllUser);
+		this.router.get('/disable', AdminController.unActiveAccount);
+		this.router.get('/delete', AdminController.deleteAccountAdmin);
+	}
+}
+
+export default new AdminRoute();
