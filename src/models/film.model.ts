b97c9e4dfa61c4d3e43f99ed2aa56a752baae77a
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-03-22 19:05:35 +0700

    add : film model

---
+++ src/models/film.model.ts
@@ -0,0 +1,32 @@
+import { Schema, Model, model } from 'mongoose';
+import { IFilmDocument } from '../interfaces/film.interface';
+import { IActorDocument } from '../interfaces/actor.interface';
+import Actor from './actor.model';
+import { schemaOption } from './user.model';
+
+export interface IFilm extends IFilmDocument {
+	getActor(): Promise<IActorDocument[]>;
+}
+
+export interface IFilmModel extends Model<IFilm> {
+	// statics here
+}
+
+const FilmSchema: Schema = new Schema(
+	{
+		_id: Schema.Types.ObjectId,
+		actor: [{ type: Schema.Types.ObjectId, ref: 'Actor' }],
+		code: { type: String, unique: true },
+		year: Date,
+		file: String,
+		info: [String],
+	},
+	schemaOption
+);
+
+FilmSchema.methods.getActor = async function (this: IFilm): Promise<IActorDocument[]> {
+	return await Actor.find({ _id: { $in: this.actor } });
+};
+
+const Film: IFilmModel = model<IFilm, IFilmModel>('Film', FilmSchema);
+export default Film;
